1d9bbf000fad5beb482709807819c95b1278f158
Jonas H <pjh.hellstrom@gmail.com>
2019-07-14 11:01:39 -0400

    working version with answer revealed between questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,57 +4,55 @@ let answer = 0;
 let winCount = 0;
 let lossCount = 0;
 let questionSet = [];
-let timeRemaining = 30;
-let levelDifficulty = 0;
+let timeRemaining = 0;
+let levelDifficulty = [];
 let apiQueryCatNum = 0;
 let ajaxResponse = [];
 let unshuffled = [];
 let shuffled = [];
-let encoded =[];
 
-const maxTime = 30;
+const maxTime = 15;
 const questionCount = 10;
 const endMsgPerfect ="Perfect score! Your score was:";
 const endMsgPass ="Well done! Your score was:";
 const endMsgFail ="Oops! Your score was:";
 
 
-//===LAUNCH GAME===
+//===LAUNCH APP===
 
 $(document).ready(function() {
     enableSfx()
     enableBgm()
+    //Difficulty input triggers category selection screen
     $("#startList > li").click(function() {
-        levelDifficulty = this.value;
-        gameLaunch();
-        return levelDifficulty;
-    })//end click listener
+        levelDifficulty = this.id;
+        selectionLaunch();
+    });//end click listener
 })
 
-
-
-function gameLaunch() {
+function selectionLaunch() {
     hideId("#startScreenWrapper")
     showId("#selectionScreenWrapper")
     bounceIn()
+    //Selection input triggers question screen
     $("#selectionList > li").click(function() {
         apiQueryCatNum = this.value;
         queryAPI();
         bounceOut();
-        setTimeout(mainGame, 700);
+        setTimeout(mainApp, 700);
         setTimeout(swoopIn, 700);
+        clearInterval(startTimer);
         return apiQueryCatNum;
     })// end click listener
     
-}// end gameLaunch()
-
+}// end selectionLaunch()
 
 
 //API query
 
 function queryAPI() {
 
-    let queryURL = `https://opentdb.com/api.php?amount=${questionCount}&category=${apiQueryCatNum}&difficulty=${levelDifficulty}&type=multiple`
+    let queryURL = `https://opentdb.com/api.php?amount=${questionCount}&category=${apiQueryCatNum}&difficulty=${levelDifficulty}&type=multiple`;
 
     $.ajax({
     url: queryURL,
@@ -66,72 +64,66 @@ function queryAPI() {
     }
 
 
-//=== LAUNCH GAME ===
-function mainGame() {
+//=== LAUNCH MAIN APP ===
+function mainApp() {
 
     hideId("#selectionScreenWrapper")
     showId("#qScreenWrapper")
     showId("#footer")
     showId("#counterWrapper")
-
     populateQuizCard()
     updateProgressBar()
-
     startTimer()
     getAnswer()
+}//end mainApp()
 
-}//end mainGame()
-
-// function restartGame() {
-//     i = 0
-//     answer = 0
-//     winCount = 0
-//     lossCount = 0
-//     hideId("#endScreenWrapper")
-//     showId("#selectionScreenWrapper")
-// }
 
 //=== MAIN FUNCTIONS ===
 
 function getAnswer() {
-
     $("#optionList > li").click(function() {
         if ( this.textContent == ajaxResponse[0].results[i].correct_answer ) {
-            winCount++
-            clearInterval(startTimer)
-            setTimeout(timeRemaining = maxTime+1, 1000)
+            winCount++;
+            revealCorrect();
+            sfxWinSound();
+            appendScoreSummaryWin();
+            clearInterval(startTimer);
+            setTimeout(timeRemaining = maxTime+1, 1500);
         } else {
-            lossCount++
-            clearInterval(startTimer)
-            setTimeout(timeRemaining = maxTime+1, 1000)
-            timeRemaining = maxTime;
+            lossCount++;
+            revealIncorrect();
+            sfxLoseSound();
+            appendScoreSummaryLose();
+            clearInterval(startTimer);
+            setTimeout(timeRemaining = maxTime+1, 1500);
         }
-        nextCard()
+        nextCard();
     })// end click listener
-
 }
 
 function nextCard() {
-    i++
-    updateProgressBar()
-    clickFreeze()
-    setTimeout(clickUnfreeze,1000)
-    cardAnimation()
-    setTimeout(populateQuizCard, 800)
+    i++;
+    updateProgressBar();
+    clickFreeze();
+    setTimeout(clickUnfreeze,1000);
+    cardAnimation();
+    setTimeout(populateQuizCard, 800);
 }
 
 function populateQuizCard() {
     if (i < ajaxResponse[0].results.length) {
         shuffleQuestions();
-        $("#question").html(`${ajaxResponse[0].results[i].question}`)
-        $("#option0").html(shuffled[0])
-        $("#option1").html(shuffled[1])
-        $("#option2").html(shuffled[2])
-        $("#option3").html(shuffled[3])
-        $("#qCategoryTag").html(ajaxResponse[0].results[i].category)
+        $("#question").html(`${ajaxResponse[0].results[i].question}`);
+        $("#option0").html(shuffled[0]);
+        $("#option1").html(shuffled[1]);
+        $("#option2").html(shuffled[2]);
+        $("#option3").html(shuffled[3]);
+        $("#qCategoryTag").html(ajaxResponse[0].results[i].category);
+        $("#counter").css("color", "#555");
+        timeRemaining = maxTime;
     }//end if
     else {
-        gameOver()
+        appEnd();
     }//end else
 }
 
@@ -165,30 +157,52 @@ function countDown() {
     if (timeRemaining > 0) {
         timeRemaining--;
     }
-    else {clearInterval(timeRemaining)
+    else {
+        if (i < ajaxResponse[0].results.length) {
+        clearInterval(timeRemaining)
+        lossCount++;
+        revealIncorrect();
+        sfxLoseSound();
+        appendScoreSummaryLose();
+        clearInterval(startTimer);
+        setTimeout(timeRemaining = maxTime, 1000);        
+        nextCard();
         $("#counter").css("color", "#555");
+        }
     }
-    if (timeRemaining < 10) {
+    if (timeRemaining < 5) {
         $("#counter").css("color", "#ff5a36");
     }
     $("#counter").text(timeRemaining);
 }
 
-function gameOver() {
+function appEnd() {
     hideId("#qScreenWrapper")
     hideId("#counterWrapper")
+    hideId("#progressBarWrapper")
     showId("#endScreenWrapper")
     if (winCount/ajaxResponse[0].results.length == 1) {
         $("#endMessage").text(endMsgPerfect)         
     }
-    else if (winCount/ajaxResponse[0].results.length > .7) {
+    else if (winCount/ajaxResponse[0].results.length > .6) {
         $("#endMessage").text(endMsgPass) 
     }
     else {
         $("#endMessage").text(endMsgFail)         
     }
-    sfxWinSound()
+    sfxEndSound();
     $("#endScore").text(`${winCount}/${ajaxResponse[0].results.length}`)      
+}
+
+
+//---MISC FUNCTIONS---
+
+function appendScoreSummaryWin() {
+    $("#scoreSummaryWrapper").append(`<div id="scoreSummaryCard" style="background-color: #5CF378"><a><i class="fas fa-check" id="scoreSummaryIcon"></i></a></div>`)
+}
+
+function appendScoreSummaryLose() {
+    $("#scoreSummaryWrapper").append(`<div id="scoreSummaryCard" style="background-color: #F37A64"><a><i class="fas fa-times" id="scoreSummaryIcon"></i></a></div>`)
 }
 
 //---SHOW/HIDE FUNCTIONS---
@@ -202,6 +216,28 @@ function hideId(selector) {
 //---RESET VARIABLE
 function resetVar() {
     selector = 0;
+};
+
+//---ANSWER REVEAL
+function revealCorrect() {
+    showId("#revealAnswerWrapper");
+    $("#answerHere").html(`${ajaxResponse[0].results[i].correct_answer} is right!`);
+    $("#revealAnswerCard").css("background-color", "#5CF378");
+    $("#revealAnswerIcon").addClass("fas fa-check");
+    setTimeout(function() {$("#answerHere").html("")}, 800);
+    setTimeout(function() {$("#revealAnswerCard").css("background-color", "unset")}, 800);
+    setTimeout(function() {$("#revealAnswerIcon").removeClass("fas fa-check")}, 800);
+    setTimeout(function() {hideId("#revealAnswerWrapper")}, 800);
+};
+function revealIncorrect() {
+    showId("#revealAnswerWrapper");
+    $("#answerHere").html(`${ajaxResponse[0].results[i].correct_answer} was the right answer!`);
+    $("#revealAnswerCard").css("background-color", "#F37A64");
+    $("#revealAnswerIcon").addClass("fas fa-times");
+    setTimeout(function() {$("#answerHere").html("")}, 800);
+    setTimeout(function() {$("#revealAnswerCard").css("background-color", "unset")}, 800);
+    setTimeout(function() {$("#revealAnswerIcon").addClass("fas fa-times")}, 800);
+    setTimeout(function() {hideId("#revealAnswerWrapper")}, 800);
 };
 
 //---ANIMATION AND SFX FUNCTIONS---
@@ -251,22 +287,33 @@ function enableSfx() {
 };
 
 function enableBgm() {
-let bgmPlaying = false;
-    $("#bgmToggle").click(function() {
-        if (!bgmPlaying) {
-            bgmPlaying = true;
-            $("#bgm")[0].play()
-        } else {
-            bgmPlaying = false;
-            $("#bgm")[0].pause()            
-        }
-});
+    $("#bgm")[0].play()
 };
+
+//FUNCTION TO PLAY/PAUSE AUDIO
+// function enableBgm() {
+// let bgmPlaying = false;
+//     $("#bgmToggle").click(function() {
+//         if (!bgmPlaying) {
+//             bgmPlaying = true;
+//             $("#bgm")[0].play()
+//         } else {
+//             bgmPlaying = false;
+//             $("#bgm")[0].pause()            
+//         }
+// });
+// };
+
 
 function sfxWinSound() {
     $("#sfxWin")[0].play()
 };
-
+function sfxLoseSound() {
+    $("#sfxLose")[0].play()
+};
+function sfxEndSound() {
+    $("#sfxEnd")[0].play()
+};
 // var clickSfx = $("#sfx")[1];
 // $(".btnHover").click(function() {
 //     clickSfx.play()
